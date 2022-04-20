26f6554ffd8dfd47a346e6a9be5c6a0b29956408
Karlis Zars 2 <zarskarlis2@gmail.com>
2022-04-20 15:44:26 +0300

    Changes from second team member

--- src/TicTacToe/Main.java
+++ src/TicTacToe/Main.java
@@ -7,6 +7,7 @@ public class Main {
     public static void main(String[] args)
     {
         //Creating Database instance
+        //Test comments from second team member
         DB db = new DB();
         Scanner scanner = new Scanner(System.in);
         char again = 'n';
