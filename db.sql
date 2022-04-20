e78cf6b3f5f4b479fa052fae89eb0947ed6e6c33
Kārlis Zars <zarskarlis@gmail.com>
2022-04-20 19:15:03 +0300

    Adding sql file

---
+++ db.sql
@@ -0,0 +1,8 @@
+
+CREATE TABLE Tictactoe (
+    Gameid int NOT NULL AUTO_INCREMENT,
+    FirstPlayer varchar(255) NOT NULL,
+    SecondPlayer varchar(255) NOT NULL,
+    Winner varchar(255) NOT NULL,
+    PRIMARY KEY (Gameid)
+);
